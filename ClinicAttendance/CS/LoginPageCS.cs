b9046672d4404ab08f610061424cfda615e4a4fa
bsb992 <bsb992@uowmail.edu.au>
2018-08-01 16:49:47 +1000

    Test

---
+++ ClinicAttendance/CS/LoginPageCS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ClinicAttendance.CS
+{
+    public class LoginPageCS : ContentPage
+    {
+        public LoginPageCS()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
